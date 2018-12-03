fe432be07a4b41b733789f8324d485bef550bbfa
Luis Lo <kwun.man.lo@gmail.com>
2018-12-03 18:56:09 +0900

    added routings and generated model for achievements

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_12_03_061112) do
+ActiveRecord::Schema.define(version: 2018_12_03_094824) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "achievements", force: :cascade do |t|
+    t.string "title"
+    t.text "description"
+    t.integer "score"
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_achievements_on_user_id"
+  end
 
   create_table "collections", force: :cascade do |t|
     t.bigint "insect_id"
@@ -50,6 +60,7 @@ ActiveRecord::Schema.define(version: 2018_12_03_061112) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "achievements", "users"
   add_foreign_key "collections", "insects"
   add_foreign_key "collections", "users"
 end
