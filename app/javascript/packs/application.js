2c88c7b89e7115e536a41729fa789598630104e6
Willem Loobuyck <willemloobuyck@msn.com>
2018-12-10 11:27:46 +0900

    removed unnecessary lines and the javascript as we are not going to use the coordinates for now

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,21 +1,6 @@
 import "bootstrap";
 
 
-
-const location = document.getElementById("location");
-
-function getLocation() {
-  if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(showPosition);
-  } else {
-    location.innerHTML = "Geolocation is not supported by this browser.";
-  }
-}
-
-function showPosition(position) {
-  location.innerHTML = "Latitude: " + position.coords.latitude +
-    "<br>Longitude: " + position.coords.longitude;
-}
 const coll = document.getElementsByClassName("collapsible");
 let i;
 
